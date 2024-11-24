472feacb59e446a24e2134d91a98f3f9b0cf22c3
Park <168292059+Parkourer10@users.noreply.github.com>
2024-11-24 16:56:47 +0530

    Update train_gpt2.py

--- config/train_gpt2.py
+++ config/train_gpt2.py
@@ -1,32 +1,19 @@
-out_dir = 'out-riyal'
-eval_interval = 1000
-eval_iters = 100
-log_interval = 10
-always_save_checkpoint = True
 
-# Weights & Biases logging
+
 wandb_log = False
-wandb_project = 'gpt-60m'
-wandb_run_name = 'gpt-60m'
+wandb_project = 'owt'
+wandb_run_name='gpt2-124M'
 
-# Dataset config
-dataset = 'openwebtext'
-gradient_accumulation_steps = 4
-batch_size = 64  # Reduce batch size if VRAM is insufficient
+batch_size = 64
 block_size = 512
+gradient_accumulation_steps = 4
 
-# Model architecture (for ~60M params)
-n_layer = 12
-n_head = 16
-n_embd = 1024  # Hidden size
-dropout = 0.1
-
-# Optimization
-learning_rate = 3e-4
-max_iters = 30000  # Increase for better convergence
+max_iters = 30000
 lr_decay_iters = 30000
-min_lr = 3e-5
-beta2 = 0.95
-warmup_iters = 1000
-device = 'cuda'
-compile = True
+
+eval_interval = 1000
+eval_iters = 200
+log_interval = 10
+
+# weight decay
+weight_decay = 1e-1
