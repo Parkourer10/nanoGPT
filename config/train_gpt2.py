e53707b57fb0b5a2cf90e50cf27c21f71cedff02
Park <168292059+Parkourer10@users.noreply.github.com>
2024-11-24 17:08:43 +0530

    Update train_gpt2.py

--- config/train_gpt2.py
+++ config/train_gpt2.py
@@ -4,6 +4,12 @@ wandb_log = False
 wandb_project = 'owt'
 wandb_run_name='gpt2-124M'
 
+n_layer = 6
+n_head = 6
+n_embd = 256
+dropout = 0.2
+
+
 batch_size = 64
 block_size = 512
 gradient_accumulation_steps = 4
